e981874df3f5f7f60a109d55e0f8a11e8fee68cd
19Pawel97 <19pawel9772@gmail.com>
2024-08-27 18:02:39 +0200

    Alternative CyclicBarrier use

--- src/main/java/org/example/ReportUpdater.java
+++ src/main/java/org/example/ReportUpdater.java
@@ -4,16 +4,18 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class ReportUpdater implements Runnable {
 
     private BigDecimal currentSum;
-    private final CountDownLatch latch;
+    private final CyclicBarrier cyclicBarrier;
 
-    public ReportUpdater(BigDecimal currentSum, CountDownLatch latch) {
+    public ReportUpdater(BigDecimal currentSum, CyclicBarrier cyclicBarrier) {
         this.currentSum = currentSum;
-        this.latch = latch;
+        this.cyclicBarrier = cyclicBarrier;
     }
 
     @Override
@@ -26,8 +28,14 @@ public class ReportUpdater implements Runnable {
         try (FileWriter writer = new FileWriter(file)) {
             writer.write(toReport.toString());
             writer.flush();
-            latch.countDown();
+            cyclicBarrier.await();
         } catch (IOException e) {
+            System.err.println(e.getMessage());
+            return;
+        } catch (BrokenBarrierException e) {
+            System.err.println(e.getMessage());
+            return;
+        } catch (InterruptedException e) {
             System.err.println(e.getMessage());
             return;
         }
